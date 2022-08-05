d9c8d4b3cc391d882b9079ce45f38f9bd3887e2a
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-08-05 20:48:00 +0100

    Update hello_world_buffer2.cpp

--- src/hello_world_buffer2.cpp
+++ src/hello_world_buffer2.cpp
@@ -1,23 +1,39 @@
-/*************************
+/*
+#                      Hello World! DPC++
+#
+# Copyright 2022 Oluwatosin Odubanjo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+
+/*************************************************************************
  *
- * HELLO WORLD in DPC++ -
+ * HELLO WORLD in DPC++ 
  *
  * VECTOR COPY
  *
  * Buffers from vector data
  *
- * @ author: Oluwatosin Odubanjo
- *
- * @date: May 17, 2022
- *
- * @time: 1:38pm
- * ************************/
+ * ************************************************************************/
+
 #include <CL/sycl.hpp>
 
 int main()
 {
         try
         {
+                // size of array
                 const int N{ 12 };
 
                 // dynamically allocate arrays
@@ -45,9 +61,21 @@ int main()
                         std::cout << "NULL CONTAINER FOUND IN main function()! Exiting...\n" << std::endl;
                         exit(EXIT_FAILURE);
                 }
+                
+                // Asynchronous error handler
+	        auto async_error_handler = [&] (cl::sycl::exception_list exceptions) {
+		        for (auto const& e : exceptions) {
+			        try{
+				        std::rethrow_exception(e);
+			        } catch(cl::sycl::exception const& e) {
+				std::cout << "Unexpected exception caught during asynchronous operation:\n" << e.what() << std::endl;
+				std::terminate();
+			        }
+		        }
+	        }; 
 
                 //select device
-                sycl::queue queue_device{sycl::gpu_selector{}};
+                sycl::queue queue_device{sycl::gpu_selector{}, async_error_handler};
 
                 // Print out device information
                 std::cout << "DEVICE = "
@@ -88,8 +116,9 @@ int main()
                 }
                 std::cout << '\n';
 
+         // synchronous error handler
         }catch(sycl::exception const &e) {
-                std::cout << e.what() << std::endl;
+                std::cout << "Unexpected exception caught during synchronous operation:\n" << e.what() << std::endl;
                 std::terminate();
         }
 
