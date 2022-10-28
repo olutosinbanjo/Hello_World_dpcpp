1ba653553aad99842c89e5bf881449627e036e2d
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-10-28 10:20:31 +0100

    Update hello_world_usm.cpp

--- src/hello_world_usm.cpp
+++ src/hello_world_usm.cpp
@@ -1,19 +1,19 @@
 /*
 #                      Hello World! DPC++
 #
-# Copyright 2022 Oluwatosin Odubanjo
+#     Copyright (c) 2022 Oluwatosin Odubanjo
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#     SPDX-License-Identifier: MIT
+# 
+*/
+
+/*    This program illustrates homogeneous programming and makes use of the unified shared memory (usm) style for data and memory management.
+#     It illustrates one of the three modes of usm allocation - malloc_shared.
+#     It features two dynamically allocated arrays, a and b, the sender and receiver array respectively.
+# 
+#     On a selected device, the data of the sender array,a, is copied into the receiver array, b, using the assignment operator, =.
+#     Finally, the data of the receiver array,b, is printed on the host device.
+#     
 */
 
 
@@ -21,7 +21,7 @@
  *
  * HELLO WORLD in DPC++ -
  *
- * ARRAY COPY
+ * ARRAY COPY WITH ASSIGNMENT OPERATOR (=)
  *
  * Unified Shared Memory
  *
